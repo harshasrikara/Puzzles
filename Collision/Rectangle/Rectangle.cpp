962381e50324c430fab7a2951f918472e77f077c
harshasrikara <harshasrikara@gmail.com>
2019-03-20 21:51:40 -0500

    Created header file
    Abstracting collision project. Created seperate file for just rectangles.

---
+++ Collision/Rectangle/Rectangle.cpp
@@ -0,0 +1,9 @@
+//
+//  Rectangle.cpp
+//  Rectangle
+//
+//  Created by Harsha Srikara on 3/20/19.
+//  Copyright © 2019 Harsha Srikara. All rights reserved.
+//
+
+#include "Rectangle.hpp"
---
+++ Collision/Rectangle/Rectangle.hpp
@@ -0,0 +1,45 @@
+//
+//  Rectangle.hpp
+//  Rectangle
+//
+//  Created by Harsha Srikara on 3/20/19.
+//  Copyright © 2019 Harsha Srikara. All rights reserved.
+//
+
+#ifndef Rectangle_hpp
+#define Rectangle_hpp
+
+#include <iostream>
+
+class Rectangle
+{
+private:
+    //variables
+    double centerX;
+    double centerY;
+    double width;
+    double length;
+    
+public:
+    //constructors
+    Rectangle();
+    Rectangle(double CX, double CY, double wid, double len);
+    
+    //setters
+    void setCenterX(double CX);
+    void setCenterY(double CY);
+    void setWidth(double wid);
+    void setLength(double len);
+    
+    //getters
+    double getCenterX() const;
+    double getCenterY() const;
+    double getWidth() const;
+    double getLength() const;
+    
+    //print
+    void print();
+    std::ostream print(std::ostream &out);
+};
+
+#endif /* Rectangle_hpp */
