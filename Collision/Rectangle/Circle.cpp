db3bd6846e956fb584b87b6c6d93752dcedd234b
harshasrikara <harshasrikara@gmail.com>
2019-05-31 12:21:02 -0500

    Added circle shape

---
+++ Collision/Rectangle/Circle.cpp
@@ -0,0 +1,9 @@
+//
+//  Circle.cpp
+//  Rectangle
+//
+//  Created by Harsha Srikara on 5/31/19.
+//  Copyright © 2019 Harsha Srikara. All rights reserved.
+//
+
+#include "Circle.hpp"
---
+++ Collision/Rectangle/Circle.hpp
@@ -0,0 +1,43 @@
+//
+//  Circle.hpp
+//  Rectangle
+//
+//  Created by Harsha Srikara on 5/31/19.
+//  Copyright © 2019 Harsha Srikara. All rights reserved.
+//
+
+#ifndef Circle_hpp
+#define Circle_hpp
+
+#include <iostream>
+
+class Circle {
+    
+private:
+    double centerX;
+    double centerY;
+    double radius;
+    
+public:
+    //constructors
+    Circle();
+    Circle(double CX, double CY, double rad);
+    
+    //setters
+    void setCenterX(double CX);
+    void setCenterY(double CY);
+    void setRadius(double wid);
+    
+    //getters
+    double getCenterX() const;
+    double getCenterY() const;
+    double getRadius() const;
+    
+    //print
+    void print();
+    void print() const;
+    std::ostream& print(std::ostream &out);
+    std::ostream& print(std::ostream &out) const;
+};
+
+#endif /* Circle_hpp */
